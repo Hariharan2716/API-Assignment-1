f90e8e4419f5b4014751e8dce93e61b388e572ec
Hariharan2716 <hariharan.m2716@gmail.com>
2025-03-17 14:03:01 +0530

    Basic express.js added to filter and map threshold frequence

--- index.js
+++ index.js
@@ -30,22 +30,36 @@
 //
 // Purpose:
 // Help teachers retrieve and analyze student performance efficiently.
-
-
 const express = require('express');
-const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
+const PORT = 3000;
 
-app.use(express.static('static'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+const students = [
+  {student_id :"1", name:"Alice Jhonson", marks:{math:98, science:90, english:90,history:90, geography:89 }, total:443 },
+  {student_id :"2", name:"Smith James", marks:{math:98, science:89, english:90,history:99, geography:99 }, total:473 },
+  {student_id :"3", name:"David Harley", marks:{math:98, science:90, english:90,history:90, geography:89 }, total:463 },
+]
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.post('/students/above-threshold',(req, res)=>{
+  const {threshold} = req.body;
 
+  if(!threshold || typeof threshold !== 'number'){
+    return res.status(400).json({error:"Invalid input. Please provide a valid threshold number. "});
 
+  }
+  const filteredStudents = students.filter(student => student.total > threshold);
+
+  res.status(200).json({
+    conunt: filteredStudents.length,
+    students: filteredStudents.map(student =>({
+      name:student.name,
+      total:student.total
+    }))
+  })
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
